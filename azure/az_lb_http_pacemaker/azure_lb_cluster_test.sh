02a143fe9aa751508ae4e4a38e812e060c61d3da
Michele Pagot <michele.pagot@suse.com>
2024-03-21 17:44:04 +0100

    More readable code in test

--- azure/az_lb_http_pacemaker/azure_lb_cluster_test.sh
+++ azure/az_lb_http_pacemaker/azure_lb_cluster_test.sh
@@ -36,33 +36,37 @@ wait_for_takeover () {
   fi
 }
 
-ssh_node () {
+ssh_node1 () {
   ssh_bastion 'ssh '"${MY_USERNAME}@${MYNAME}-vm-01 $*"
+}
+
+ssh_node2 () {
+  ssh_bastion 'ssh '"${MY_USERNAME}@${MYNAME}-vm-02 $*"
 }
 
 
 test_step "Check cluster preconditions"
-ssh_node 'sudo crm configure show' | grep -c cli-prefer- | grep 0 || test_die "Cluster already have some cli-prefer- and it should not at the beginning of the test"
-ssh_node 'sudo crm status'
+ssh_node1 'sudo crm configure show' | grep -c cli-prefer- | grep 0 || test_die "Cluster already have some cli-prefer- and it should not at the beginning of the test"
+ssh_node1 'sudo crm status'
 
 test_step "Check ip address preconditions"
-ssh_node 'ip a show eth0' | grep ${MY_FIP} || test_die "VirtualIP ${MY_FIP} should be on ${MYNAME}-vm-01"
-ssh_bastion 'ssh '"${MY_USERNAME}@${MYNAME}-vm-02"' ip a show eth0'
+ssh_node1 'ip a show eth0' | grep ${MY_FIP} || test_die "VirtualIP ${MY_FIP} should be on ${MYNAME}-vm-01"
+ssh_node2 'ip a show eth0'
 test_web "${MYNAME}-vm-01"
 
 
 test_step "Move the IpAddr2 resource to VM2"
-ssh_node 'sudo crm resource move '"${MY_MOVE_RES} ${MYNAME}-vm-02" || test_die "Error in resource move"
+ssh_node1 'sudo crm resource move '"${MY_MOVE_RES} ${MYNAME}-vm-02" || test_die "Error in resource move"
 sleep 30
 
 test_step "Check cluster immediately after the move"
-ssh_node 'sudo crm configure show' | grep -c cli-prefer- | grep 1 || test_die "Cluster should now have one cli-prefer-"
-ssh_node 'sudo crm configure show' | grep -E "cli-prefer-.*${MYNAME}-vm-02" || test_die "Cluster should now have one cli-prefer- with ${MYNAME}-vm-02"
-ssh_node 'sudo crm status'
+ssh_node1 'sudo crm configure show' | grep -c cli-prefer- | grep 1 || test_die "Cluster should now have one cli-prefer-"
+ssh_node1 'sudo crm configure show' | grep -E "cli-prefer-.*${MYNAME}-vm-02" || test_die "Cluster should now have one cli-prefer- with ${MYNAME}-vm-02"
+ssh_node1 'sudo crm status'
 
 test_step "Check ip address immediately after the move"
-ssh_node 'ip a show eth0'
-ssh_bastion 'ssh '"${MY_USERNAME}@${MYNAME}-vm-02"' ip a show eth0' | grep ${MY_FIP} || test_die "VirtualIP ${MY_FIP} should be on ${MYNAME}-vm-02"
+ssh_node1 'ip a show eth0'
+ssh_node2 'ip a show eth0' | grep ${MY_FIP} || test_die "VirtualIP ${MY_FIP} should be on ${MYNAME}-vm-02"
 
 wait_for_takeover "${MYNAME}-vm-02"
 
@@ -72,27 +76,27 @@ sleep 30
 test_web "${MYNAME}-vm-02"
 
 test_step "Move back the IpAddr2 resource to VM1"
-ssh_node 'sudo crm resource move '"${MY_MOVE_RES} ${MYNAME}-vm-01" || test_die "Error in resource move"
+ssh_node1 'sudo crm resource move '"${MY_MOVE_RES} ${MYNAME}-vm-01" || test_die "Error in resource move"
 sleep 30
 
 test_step "Check cluster immediately after the move"
-ssh_node 'sudo crm configure show' | grep -c cli-prefer- | grep 1 || test_die "Cluster should now have one cli-prefer-"
-ssh_node 'sudo crm configure show' | grep -E "cli-prefer-.*${MYNAME}-vm-01" || test_die "Cluster should now have one cli-prefer- with ${MYNAME}-vm-01"
-ssh_node 'sudo crm status'
+ssh_node1 'sudo crm configure show' | grep -c cli-prefer- | grep 1 || test_die "Cluster should now have one cli-prefer-"
+ssh_node1 'sudo crm configure show' | grep -E "cli-prefer-.*${MYNAME}-vm-01" || test_die "Cluster should now have one cli-prefer- with ${MYNAME}-vm-01"
+ssh_node1 'sudo crm status'
 
 test_step "Check ip address immediately after the move"
-ssh_node 'ip a show eth0' | grep ${MY_FIP} || test_die "VirtualIP ${MY_FIP} should be on ${MYNAME}-vm-01"
-ssh_bastion 'ssh '"${MY_USERNAME}@${MYNAME}-vm-02"' ip a show eth0'
+ssh_node1 'ip a show eth0' | grep ${MY_FIP} || test_die "VirtualIP ${MY_FIP} should be on ${MYNAME}-vm-01"
+ssh_node2 'ip a show eth0'
 
 wait_for_takeover "${MYNAME}-vm-01"
 
 test_step "Clear all location constrain used during the test"
-ssh_node 'sudo crm resource clear '"${MY_MOVE_RES}"
+ssh_node1 'sudo crm resource clear '"${MY_MOVE_RES}"
 sleep 30
 
 test_step "Check cluster after the clear"
-ssh_node 'sudo crm configure show' | grep -c cli-prefer- | grep 0 || test_die "Cluster should no more have some cli-prefer-"
-ssh_node 'sudo crm status'
+ssh_node1 'sudo crm configure show' | grep -c cli-prefer- | grep 0 || test_die "Cluster should no more have some cli-prefer-"
+ssh_node1 'sudo crm status'
 
 
 # Slow down, take a break, then check again, nothing should be changed.
